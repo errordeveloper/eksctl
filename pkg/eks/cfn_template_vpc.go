a5dca6655480008e874f46efdc8904ebb49c47a8
Ilya Dmitrichenko <errordeveloper@gmail.com>
2018-07-22 12:29:17 +0100

    Some refactoring

--- pkg/eks/cfn_template_vpc.go
+++ pkg/eks/cfn_template_vpc.go
@@ -6,29 +6,41 @@ import (
 	"github.com/awslabs/goformation/cloudformation"
 )
 
-func newParameter(t *cloudformation.Template, name, valueType, defaultValue string) interface{} {
-	p := map[string]string{"Type": valueType}
-	if defaultValue != "" {
-		p["Default"] = defaultValue
-	}
-	t.Parameters[name] = p
-	return newRef(name)
-}
+// func newParameter(t *cloudformation.Template, name, valueType, defaultValue string) interface{} {
+// 	p := map[string]string{"Type": valueType}
+// 	if defaultValue != "" {
+// 		p["Default"] = defaultValue
+// 	}
+// 	t.Parameters[name] = p
+// 	return newRef(name)
+// }
 
-func newStringParameter(t *cloudformation.Template, name, defaultValue string) interface{} {
-	return newParameter(t, name, "String", defaultValue)
-}
+// func newStringParameter(t *cloudformation.Template, name, defaultValue string) interface{} {
+// 	return newParameter(t, name, "String", defaultValue)
+// }
+
+// func newSub(sub string) interface{} {
+// 	return map[string]string{"Sub": sub}
+// }
 
 func newRef(refName string) interface{} {
 	return map[string]string{"Ref": refName}
 }
 
-func newSub(sub string) interface{} {
-	return map[string]string{"Sub": sub}
+func newResource(t *cloudformation.Template, name string, resource interface{}) interface{} {
+	t.Resources[name] = resource
+	return newRef(name)
 }
 
-func addResourcesForVPC(t *cloudformation.Template, stackName string, globalCIDR *net.IPNet, subnets map[string]*net.IPNet) {
-	t.Resources["VPC"] = &cloudformation.UntypedAWSEC2VPC{
+type resourceRefsForVPC struct {
+	VPC            interface{}
+	Subnets        []interface{}
+	SecurityGroups []interface{}
+}
+
+func addResourcesForVPC(t *cloudformation.Template, stackName string, globalCIDR *net.IPNet, subnets map[string]*net.IPNet) *resourceRefsForVPC {
+	refs := &resourceRefsForVPC{}
+	refs.VPC = newResource(t, "VPC", &cloudformation.UntypedAWSEC2VPC{
 		CidrBlock:          globalCIDR.String(),
 		EnableDnsSupport:   true,
 		EnableDnsHostnames: true,
@@ -36,50 +48,50 @@ func addResourcesForVPC(t *cloudformation.Template, stackName string, globalCIDR
 		// 	Key:   "Name",
 		// 	Value: stackName + ".VPC",
 		// }},
-	}
-	refVPC := newRef("VPC")
+	})
 
-	t.Resources["InternetGateway"] = &cloudformation.AWSEC2InternetGateway{}
-	refIG := newRef("InternetGateway")
-	t.Resources["VPCGatewayAttachment"] = &cloudformation.UntypedAWSEC2VPCGatewayAttachment{
+	refIG := newResource(t, "InternetGateway", &cloudformation.AWSEC2InternetGateway{})
+	newResource(t, "VPCGatewayAttachment", &cloudformation.UntypedAWSEC2VPCGatewayAttachment{
 		InternetGatewayId: refIG,
-		VpcId:             refVPC,
-	}
+		VpcId:             refs.VPC,
+	})
 
-	t.Resources["RouteTable"] = &cloudformation.UntypedAWSEC2RouteTable{
-		VpcId: refVPC,
+	refRT := newResource(t, "RouteTable", &cloudformation.UntypedAWSEC2RouteTable{
+		VpcId: refs.VPC,
 		// Tags: []cloudformation.Tag{
 		// 	{Key: "Name", Value: "Public Subnets"},
 		// 	{Key: "Network", Value: "Public"},
 		// },
-	}
-	refRT := newRef("RouteTable")
+	})
 
-	t.Resources["Route"] = &cloudformation.UntypedAWSEC2Route{
+	newResource(t, "Route", &cloudformation.UntypedAWSEC2Route{
 		RouteTableId:         refRT,
 		DestinationCidrBlock: "0.0.0.0/0",
 		GatewayId:            newRef("InternetGateway"),
-	}
+	})
 
 	for az, subnet := range subnets {
-		name := "Subnet_" + az
-		t.Resources[name] = &cloudformation.UntypedAWSEC2Subnet{
+		refSubnet := newResource(t, "Subnet_"+az, &cloudformation.UntypedAWSEC2Subnet{
 			AvailabilityZone: az,
 			CidrBlock:        subnet.String(),
-			VpcId:            refVPC,
+			VpcId:            refs.VPC,
 			// Tags: []cloudformation.Tag{{
 			// 	Key:   "Name",
 			// 	Value: stackName + "." + name,
 			// }},
-		}
-		t.Resources["RouteTableAssociation_"+az] = &cloudformation.UntypedAWSEC2SubnetRouteTableAssociation{
-			SubnetId:     newRef(name),
+		})
+		newResource(t, "RouteTableAssociation_"+az, &cloudformation.UntypedAWSEC2SubnetRouteTableAssociation{
+			SubnetId:     refSubnet,
 			RouteTableId: refRT,
-		}
+		})
+		refs.Subnets = append(refs.Subnets, refSubnet)
 	}
 
-	t.Resources["ControlPlaneSecurityGroup"] = &cloudformation.UntypedAWSEC2SecurityGroup{
+	refSG := newResource(t, "ControlPlaneSecurityGroup", &cloudformation.UntypedAWSEC2SecurityGroup{
 		GroupDescription: "Cluster communication with worker nodes",
-		VpcId:            refVPC,
-	}
+		VpcId:            refs.VPC,
+	})
+	refs.SecurityGroups = []interface{}{refSG}
+
+	return refs
 }
